4f01fc942a146295178a15a379cfc0980348d247
Guriel <gurielamp@zeniuseducation.com>
2015-08-13 06:59:16 +0700

    show, hide description

--- js/zmm.js
+++ js/zmm.js
@@ -5,3 +5,27 @@ $(".x-menu").click(function () {
 $(".s1, .s2").click(function () {
     $(".nav-opt").slideUp("fast");
 });
+
+$('.show-7ktsp').click(function() {
+    $('.xpedia7ktsp-desc').slideDown();
+    $('.show-7ktsp').hide();
+    $('.hide-7ktsp').show();
+});
+
+$('.hide-7ktsp').click(function() {
+    $('.xpedia7ktsp-desc').slideUp();
+    $('.hide-7ktsp').hide();
+    $('.show-7ktsp').show();
+});
+
+$('.show-7k13').click(function() {
+    $('.xpedia7k13-desc').slideDown();
+    $('.show-7k13').hide();
+    $('.hide-7k13').show();
+});
+
+$('.hide-7k13').click(function() {
+    $('.xpedia7k13-desc').slideUp();
+    $('.hide-7k13').hide();
+    $('.show-7k13').show();
+});
