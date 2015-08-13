f5c754d15d103357866cb284664d42848eba9e94
Guriel <gurielamp@zeniuseducation.com>
2015-08-13 07:29:39 +0700

    update xpedia smp

--- js/zmm.js
+++ js/zmm.js
@@ -29,3 +29,51 @@ $('.hide-7k13').click(function() {
     $('.hide-7k13').hide();
     $('.show-7k13').show();
 });
+
+$('.show-8ktsp').click(function() {
+    $('.xpedia8ktsp-desc').slideDown();
+    $('.show-8ktsp').hide();
+    $('.hide-8ktsp').show();
+});
+
+$('.hide-8ktsp').click(function() {
+    $('.xpedia8ktsp-desc').slideUp();
+    $('.hide-8ktsp').hide();
+    $('.show-8ktsp').show();
+});
+
+$('.show-8k13').click(function() {
+    $('.xpedia8k13-desc').slideDown();
+    $('.show-8k13').hide();
+    $('.hide-8k13').show();
+});
+
+$('.hide-8k13').click(function() {
+    $('.xpedia8k13-desc').slideUp();
+    $('.hide-8k13').hide();
+    $('.show-8k13').show();
+});
+
+$('.show-9ktsp').click(function() {
+    $('.xpedia9ktsp-desc').slideDown();
+    $('.show-9ktsp').hide();
+    $('.hide-9ktsp').show();
+});
+
+$('.hide-9ktsp').click(function() {
+    $('.xpedia9ktsp-desc').slideUp();
+    $('.hide-9ktsp').hide();
+    $('.show-9ktsp').show();
+});
+
+$('.show-9k13').click(function() {
+    $('.xpedia9k13-desc').slideDown();
+    $('.show-9k13').hide();
+    $('.hide-9k13').show();
+});
+
+$('.hide-9k13').click(function() {
+    $('.xpedia9k13-desc').slideUp();
+    $('.hide-9k13').hide();
+    $('.show-9k13').show();
+});
