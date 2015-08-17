abaabedc205593c6f352c78e4d25fead756f5260
Guriel <gurielamp@zeniuseducation.com>
2015-08-18 06:48:24 +0700

    SMA products - JS

--- js/zmm.js
+++ js/zmm.js
@@ -77,3 +77,147 @@ $('.hide-9k13').click(function() {
     $('.hide-9k13').hide();
     $('.show-9k13').show();
 });
+
+$('.show-10ktsp').click(function() {
+    $('.xpedia10ktsp-desc').slideDown();
+    $('.show-10ktsp').hide();
+    $('.hide-10ktsp').show();
+});
+
+$('.hide-10ktsp').click(function() {
+    $('.xpedia10ktsp-desc').slideUp();
+    $('.hide-10ktsp').hide();
+    $('.show-10ktsp').show();
+});
+
+$('.show-10ipak13').click(function() {
+    $('.xpedia10ipak13-desc').slideDown();
+    $('.show-10ipak13').hide();
+    $('.hide-10ipak13').show();
+});
+
+$('.hide-10ipak13').click(function() {
+    $('.xpedia10ipak13-desc').slideUp();
+    $('.hide-10ipak13').hide();
+    $('.show-10ipak13').show();
+});
+
+$('.show-10ipsk13').click(function() {
+    $('.xpedia10ipsk13-desc').slideDown();
+    $('.show-10ipsk13').hide();
+    $('.hide-10ipsk13').show();
+});
+
+$('.hide-10ipsk13').click(function() {
+    $('.xpedia10ipsk13-desc').slideUp();
+    $('.hide-10ipsk13').hide();
+    $('.show-10ipsk13').show();
+});
+
+$('.show-11ipaktsp').click(function() {
+    $('.xpedia11ipaktsp-desc').slideDown();
+    $('.show-11ipaktsp').hide();
+    $('.hide-11ipaktsp').show();
+});
+
+$('.hide-11ipaktsp').click(function() {
+    $('.xpedia11ipaktsp-desc').slideUp();
+    $('.hide-11ipaktsp').hide();
+    $('.show-11ipaktsp').show();
+});
+
+$('.show-11ipsktsp').click(function() {
+    $('.xpedia11ipsktsp-desc').slideDown();
+    $('.show-11ipsktsp').hide();
+    $('.hide-11ipsktsp').show();
+});
+
+$('.hide-11ipsktsp').click(function() {
+    $('.xpedia11ipsktsp-desc').slideUp();
+    $('.hide-11ipsktsp').hide();
+    $('.show-11ipsktsp').show();
+});
+
+$('.show-11ipak13').click(function() {
+    $('.xpedia11ipak13-desc').slideDown();
+    $('.show-11ipak13').hide();
+    $('.hide-11ipak13').show();
+});
+
+$('.hide-11ipak13').click(function() {
+    $('.xpedia11ipak13-desc').slideUp();
+    $('.hide-11ipak13').hide();
+    $('.show-11ipak13').show();
+});
+
+$('.show-11ipsk13').click(function() {
+    $('.xpedia11ipsk13-desc').slideDown();
+    $('.show-11ipsk13').hide();
+    $('.hide-11ipsk13').show();
+});
+
+$('.hide-11ipsk13').click(function() {
+    $('.xpedia11ipsk13-desc').slideUp();
+    $('.hide-11ipsk13').hide();
+    $('.show-11ipsk13').show();
+});
+
+$('.show-12ipaktsp').click(function() {
+    $('.xpedia12ipaktsp-desc').slideDown();
+    $('.show-12ipaktsp').hide();
+    $('.hide-12ipaktsp').show();
+});
+
+$('.hide-12ipaktsp').click(function() {
+    $('.xpedia12ipaktsp-desc').slideUp();
+    $('.hide-12ipaktsp').hide();
+    $('.show-12ipaktsp').show();
+});
+
+$('.show-12ipsktsp').click(function() {
+    $('.xpedia12ipsktsp-desc').slideDown();
+    $('.show-12ipsktsp').hide();
+    $('.hide-12ipsktsp').show();
+});
+
+$('.hide-12ipsktsp').click(function() {
+    $('.xpedia12ipsktsp-desc').slideUp();
+    $('.hide-12ipsktsp').hide();
+    $('.show-12ipsktsp').show();
+});
+
+$('.show-12ipak13').click(function() {
+    $('.xpedia12ipak13-desc').slideDown();
+    $('.show-12ipak13').hide();
+    $('.hide-12ipak13').show();
+});
+
+$('.hide-12ipak13').click(function() {
+    $('.xpedia12ipak13-desc').slideUp();
+    $('.hide-12ipak13').hide();
+    $('.show-12ipak13').show();
+});
+
+$('.show-12ipsk13').click(function() {
+    $('.xpedia12ipsk13-desc').slideDown();
+    $('.show-12ipsk13').hide();
+    $('.hide-12ipsk13').show();
+});
+
+$('.hide-12ipsk13').click(function() {
+    $('.xpedia12ipsk13-desc').slideUp();
+    $('.hide-12ipsk13').hide();
+    $('.show-12ipsk13').show();
+});
+
+$('.show-alumni').click(function() {
+    $('.xpediaalumni-desc').slideDown();
+    $('.show-alumni').hide();
+    $('.hide-alumni').show();
+});
+
+$('.hide-alumni').click(function() {
+    $('.xpediaalumni-desc').slideUp();
+    $('.hide-alumni').hide();
+    $('.show-alumni').show();
+});
